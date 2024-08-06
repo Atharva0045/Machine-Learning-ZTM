e0879b9400162fb2d8eda67ab44204c7f221ce18
Atharva Aher <atharva758876@gmail.com>
2024-08-06 23:07:51 +0530

    Add Tuple Data Structure

---
+++ Path 2 (Python)/Part 1/d_types/tuples/main.py
@@ -0,0 +1,8 @@
+# Tuples are immtable lists..
+# we can do any list methods that require no modifications.. i.e. access, find, checking, etc.
+# one can also do slicing
+# tuples can also act as key in dictionaries as they are immutable
+
+example = (10, 20, 30, 40)
+
+print(example[0:3])
