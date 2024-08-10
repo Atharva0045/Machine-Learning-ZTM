c12d1cf031e99a4879c1418c3a7951c96fc72653
Atharva Aher <atharva758876@gmail.com>
2024-08-10 15:13:32 +0530

    Implement main() in python

---
+++ Mini Projects/skip_global.py
@@ -0,0 +1,8 @@
+def main():
+    '''
+    This is Docstring: It shows the info about the function, shows when you hover over the function call
+    '''
+    print("This is inside the main function!")
+
+if __name__ == "__main__":
+    main()
